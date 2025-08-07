c27f50c8657a20d9d01aa3432d39da47b69e294c
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-08-07 20:53:21 -0300

    primeira versão

---
+++ CadastroDeUsuario.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CadastroDeUsuario {
+    public static void main(String[] args) {
+        // Criação do frame (janela)
+        JFrame frame = new JFrame("Cadastro de Usuário!");
+        frame.setSize(300,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar e não continuar processando
+        frame.setLayout(new FlowLayout()); /// FlowLayout: organiza os componentes em uma linha horizontal e quebra automaticamente para a próxima linha
+        
+        //Rótulos
+        JLabel labelNome = new JLabel("Nome: ");
+        JLabel labelEmail = new JLabel("E-mail: ");
+        JLabel labelIdade = new JLabel("Idade: ");
+
+        //Campos de texto
+        JTextField campoNome = new JTextField(30);
+        JTextField campoEmail = new JTextField(30);
+        JTextField campoIdade = new JTextField(3);
+
+        //Botão
+        JButton botaoCadastrar = new JButton("Cadastrar");
+
+        //Ação do botão
+        botaoCadastrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(frame, " Usuário Cadastrado: \n" + "Nome: " + campoNome.getText() + "\n E-mail: " + campoEmail.getText() + "\n Idade:" + campoIdade.getText() );
+            }
+        });
+
+        //Adicionando componentes ao Frame
+        frame.add(labelNome);
+        frame.add(campoNome);
+
+        frame.add(labelEmail);
+        frame.add(campoEmail);
+
+        frame.add(labelIdade);
+        frame.add(campoIdade);
+
+        frame.add(botaoCadastrar);
+
+        //Exibir a janela 
+        frame.setVisible(true);
+        
+    }
+
+}
